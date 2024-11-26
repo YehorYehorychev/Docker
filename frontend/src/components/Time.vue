ead2d2c8cc68d6d7f668d71d1d82c9a130e12b55
Yehor Yehorychev <egor57899@gmail.com>
2024-11-26 10:55:58 -0800

    docker-compose.yml | env variables have been added for api service

--- frontend/src/components/Time.vue
+++ frontend/src/components/Time.vue
@@ -29,7 +29,7 @@ export default {
 <template>
   <div class="greetings">
     <h1 class="green">{{ currentTime }}</h1>
-    <h3>Click on the button to save this in DB</h3>
+    <h3>Click on the button to save timestamps in DB</h3>
     <button @click="saveTime">Save Current Timestamp</button>
     <h3 v-if="savedTimes.length && showSavedTimes">
       Saved:
